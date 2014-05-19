0ef8260d5b8d02d5702fba24c538948f4110eba1
Yossi Rafelson <yossi.rafelson@gmail.com>
2014-05-19 11:58:26 -0700

    oops

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import gmail # https://github.com/charlierguo/gmail
 import sys
 import secret
-import datetime
+from datetime import date
 
 import MySQLdb
 db = MySQLdb.connect(**secret.db_credentials)
@@ -25,7 +25,7 @@ def scrape():
              'Ingress Portal Game Rejected',)
 
     last_run = max(exec_mysql('SELECT max(ping), max(pong) FROM portals2;')[0])
-    if not last_run: last_run = datetime.date(2012, 11, 15) # closed beta begin date
+    if not last_run: last_run = date(2012, 11, 15) # closed beta begin date
     data = exec_mysql('SELECT ping, pong, `name`, `status` FROM portals2;')
 
     print 'logging into %s@gmail.com' % username
